f51f533d20cf2c47d53a15f43eb88ec052396a8b
Jazzmyr <miriamlopez.v98@gmail.com>
2022-02-06 15:18:51 -0600

    DOm

--- 02-introAJS/06-funciones/js/index.js
+++ 02-introAJS/06-funciones/js/index.js
@@ -1,29 +1,3 @@
-var texto;
-
-texto = prompt('Ingrese una oración');
-
-function palindromo(texto){
-
-    var cadenaPrincipal = texto.toLowerCase().toUpperCase();
-
-    var letrasEspacios = cadenaPrincipal.split('');
-
-    var cadenaSinEspacios = '';
-    for(i in letrasEspacio){
-        if(letrasEspacios[i] != " "){
-            cadenaSinEspacios += letrasEspacios[i];
-
-        }
-    }
-
-    var letras = cadenaSinEspacios;
-    var reverse = cadenaSinEspacios.split('').reverse('');
-
-    var igual = 
-}
-
-    
-
 //--------------------Pares o impares-------------------
 function pares(){
     var num = Number(prompt('Ingressa un número'));
---
+++ 02-introAJS/09-domJS/js/index.js
@@ -0,0 +1,41 @@
+let txtNombre, añoNac, edad, añoActual;
+
+function capturaNombre()
+{
+    txtNombre = document.getElementById('nombre')
+    
+    alert(`Tu nombre es ${txtNombre.value}`)
+}
+
+function calcularEdad()
+{
+    let fecha = new Date();
+    añoNac = Number(document.getElementById('fecha').value.slice(0,4));
+    añoActual = fecha.getFullYear();   
+
+    if(añoActual < añoNac)
+    {
+        alert('Elige un año de nacimiento válido')
+    }
+    else
+    {
+        edad = añoActual - añoNac;
+
+        document.getElementById('edad').innerText = `Tu edad es ${edad}`
+    }
+
+}
+
+document.getElementById('calcular').addEventListener('click', calcularEdad);
+
+function cambiarColor()
+{
+    if(document.getElementById('cuerpo').style.backgroundColor = '#90caf9' )
+    {
+        document.getElementById('cuerpo').style.backgroundColor = '#b2dfdb';
+    }
+    else if(document.getElementById('cuerpo').style.backgroundColor = '#b2dfdb' )
+    {
+        document.getElementById('cuerpo').style.backgroundColor = '#90caf9';
+    }
+}
